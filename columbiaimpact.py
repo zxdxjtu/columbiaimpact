618a3b86f574d95459895795e40321e074dbf5c1
Shen Zhu <shenzhu0127@gmail.com>
2016-11-13 09:33:56 -0500

    add male page

--- columbiaimpact.py
+++ columbiaimpact.py
@@ -3,6 +3,17 @@ from flask import render_template, redirect
 from flask import request
 import json
 import requests
+import pandas as pd
+import os
+import sys
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+# Find root directory
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+APP_STATIC = os.path.join(APP_ROOT, 'static')
 
 
 app = Flask(__name__)
@@ -18,8 +29,10 @@ def hello_world():
 
 @app.route('/male')
 def male():
+    # Read data from file
+    allBathrooms, maleBathrooms, femaleBathrooms, unisexBathrooms = read_data()
 
-    context = dict()
+    context = dict(maleBathrooms = maleBathrooms[:30])
 
     return render_template("male.html", **context)
 
@@ -71,6 +84,45 @@ def add_bathroom():
     context = dict()
 
     return render_template("addBathroom.html", **context)
+
+
+def read_data():
+    data = pd.read_csv(os.path.join(APP_STATIC, 'ToiletMap.csv'))
+    dataframe = pd.DataFrame(data, columns=['ToiletID', 'Address1', 'Male', 'Female', 'Unisex', 'AccessibleMale',
+                                        'OpeningHoursSchedule', 'BabyChange', 'PaymentRequired', 'Latitude',
+                                        'Longitude', 'rating'])
+    allBathrooms = []
+    maleBathRooms = []
+    femaleBathrooms = []
+    unisexBathrooms = []
+
+    for index, row in dataframe.iterrows():
+        toiletID = row['ToiletID']
+        AddressOne = row['Address1']
+        Male = row['Male']
+        Female = row['Female']
+        Unisex = row['Unisex']
+        WheelchairFriendly = row['AccessibleMale']
+        OpeningHours = row['OpeningHoursSchedule']
+        BabyChange = row['BabyChange']
+        PaymentRequired = row['PaymentRequired']
+        Latitude = row['Latitude']
+        Longitude = row['Longitude']
+        rating = row['rating']
+
+        tempDict = dict(toiletID = toiletID, addressOne = AddressOne, male = Male, female = Female, unisex = Unisex,
+                        wheelchairFriendly = WheelchairFriendly, openingHours = OpeningHours, babyChange = BabyChange,
+                        paymentRequired = PaymentRequired, latitude = Latitude, longitude = Longitude, rating = rating)
+
+        if tempDict['male'] == True:
+            maleBathRooms.append(tempDict)
+        if tempDict['female'] == True:
+            femaleBathrooms.append(tempDict)
+        if tempDict['unisex'] == True:
+            unisexBathrooms.append(tempDict)
+        allBathrooms.append(tempDict)
+
+    return allBathrooms, maleBathRooms, femaleBathrooms, unisexBathrooms
 
 
 if __name__ == '__main__':
