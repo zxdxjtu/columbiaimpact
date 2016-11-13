bfa67c5c032036b4ddeeeb3bbe4bb41ad1160fdd
Shen Zhu <shenzhu0127@gmail.com>
2016-11-12 21:27:20 -0500

    version 1, add template

--- columbiaimpact.py
+++ columbiaimpact.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask import render_template, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+
+    # A flag indicating if user has logged in
+    userLoggedIn = False
+
+    context = dict(userLoggedIn = userLoggedIn)
+
+    return render_template("index.html", **context)
 
 
 if __name__ == '__main__':
